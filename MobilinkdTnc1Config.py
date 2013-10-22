850b155edc16af8e139fc837f122fd34a3a09440
Rob Riggs <rob@pangalactic.org>
2013-10-21 21:18:33 -0500

    Update version.

--- MobilinkdTnc1Config.py
+++ MobilinkdTnc1Config.py
@@ -3,8 +3,10 @@
 import sys
 import os
 import gi
+
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk,GdkPixbuf,GObject,Pango,Gdk
+
 import serial
 import serial.tools
 import serial.tools.list_ports
@@ -14,6 +16,7 @@ import threading
 import math
 import time
 from StringIO import StringIO
+from Avr109 import Avr109
 
 def comports():
     if os.name == 'posix':
@@ -23,127 +26,6 @@ def comports():
         print [x for x in serial.tools.list_ports.comports()]
         return serial.tools.list_ports.comports()
 
-class Avr109(object):
-    """AVR109 firmware upload protocol."""
-    
-    def __init__(self, reader, writer, filename):
-        self.sio_reader = reader
-        self.sio_writer = writer
-        self.filename = filename
-        
-    def start(self):
-        self.sio_writer.write('\033')
-        self.sio_writer.flush()
-        buf = self.sio_reader.read(10)
-        print buf
-    
-    def do_upload(self):
-        pass
-    
-    def verify_command_sent(self, cmd):
-        c = self.sio_reader.read(1)
-        if c != '\r':
-            raise IOError("programmer did not respond to command: %s" % cmd)
-    
-    def chip_erase(self):
-        self.sio_writer.write('e')
-        self.sio_writer.flush()
-        self.verify_command_sent("chip erase")
-    
-    def enter_program_mode(self):
-        self.sio_writer.write('P')
-        self.sio_writer.flush()
-        self.verify_command_sent("enter program mode")
-        
-    def leave_program_mode(self):
-        self.sio_writer.write('L')
-        self.sio_writer.flush()
-        self.verify_command_sent("leave program mode")
-   
-    def exit_bootloader(self):
-        self.sio_writer.write('E')
-        self.sio_writer.flush()
-        self.verify_command_sent("exit bootloader")
-   
-    def initialize(self):
-    
-        loader = ""
-        count = 0
-        while loader != 'XBoot++':
-            self.start()
-            # Bootloader
-            self.sio_writer.write('S')
-            self.sio_writer.flush()
-            loader = self.sio_reader.read(7)
-            print "loader", loader
-            count += 1
-            if count == 5:
-                raise IOError("bootloader did not respond")
-        
-        # Software Version
-        self.sio_writer.write('V')
-        self.sio_writer.flush()
-        sw_version = self.sio_reader.read(2)
-        
-        # Hardware Version
-        self.sio_writer.write('V')
-        self.sio_writer.flush()
-        hw_version = self.sio_reader.read(2)
-        
-        # Programmer Type
-        self.sio_writer.write('p')
-        self.sio_writer.flush()
-        programmer_type = self.sio_reader.read(1)
-        
-        # Auto-increment support
-        self.sio_writer.write('p')
-        self.sio_writer.flush()
-        if self.sio_reader.read(1) == 'Y':
-            auto_increment = True
-        else:
-            auto_increment = False
-        
-        # Block-mode support
-        buffer_size = 0
-        self.sio_writer.write('b')
-        self.sio_writer.flush()
-        if self.sio_reader.read(1) == 'Y':
-            block_mode = True
-        else:
-            block_mode = False
-
-        if block_mode:
-            tmp = self.sio_reader.read(2)
-            buffer_size = ord(tmp[0]) * 256 + ord(tmp[1])
-        
-        # Device Code
-        self.sio_writer.write('t')
-        self.sio_writer.flush()
-        device_list = []
-        while True:
-            device = self.sio_reader.read(1)      
-            if ord(device[0]) == 0: break
-            device_list.append(device)
-        
-        # Read Signature
-        self.sio_writer.write('s')
-        self.sio_writer.flush()
-        signature = self.sio_reader.read(3)
- 
-        print "  Found programmer: Id = '%s'; type = '%s'" % (loader, programmer_type)
-        print "  Software Version: %d.%d" % (ord(sw_version[0]) - 48, ord(sw_version[1]) - 48)
-        print "  Hardware Version: %d.%d" % (ord(hw_version[0]) - 48, ord(hw_version[1]) - 48)
-        print "Has auto-increment: %s" % (str(auto_increment))
-        print "    Has block-mode: %s (size = %d)" % (str(block_mode), buffer_size)
-        print "  Device Signature: %02x %02x %02x" % (ord(signature[0]),ord(signature[1]),ord(signature[2]))
-        
-        if signature != '\x0f\x95\x1e':
-            raise ValueError("Bad device signature. Not an AVR ATmega 328P.")
-        
-        # Select Device
-        self.sio_writer.write('T' + device_list[0])
-        self.sio_writer.flush()
-        self.sio_reader.read(1)
 
 class KissData(object):
 
@@ -266,19 +148,19 @@ class TncModel(object):
     SET_DUPLEX = "\05%c"
     GET_DUPLEX = "\06\045"
     
-    SET_OUTPUT_VOLUME = "\06\01%c"
-    GET_OUTPUT_VOUME = "\06\014"
-    SET_INPUT_VOLUME = "\06\02%c" # UNUSED
-    GET_INPUT_VOUME = "\06\015"   # UNUSED
-    SET_SQUELCH_LEVEL = "\06\03%c"
+    SET_OUTPUT_VOLUME="\06\01%c"
+    GET_OUTPUT_VOUME="\06\014"
+    SET_INPUT_VOLUME="\06\02%c" # UNUSED
+    GET_INPUT_VOUME="\06\015"   # UNUSED
+    SET_SQUELCH_LEVEL="\06\03%c"
     
-    POLL_VOLUME = "\06\04"        # One value
-    STREAM_VOLUME = "\06\05"      # Stream continuously
+    POLL_VOLUME="\06\04"        # One value
+    STREAM_VOLUME="\06\05"      # Stream continuously
 
-    SEND_MARK = "\x06\x07"
-    SEND_SPACE = "\x06\x08"
-    SEND_BOTH = "\x06\x09"
-    STOP_TX = "\x06\x0a"
+    SEND_MARK="\06\07"
+    SEND_SPACE="\06\010"
+    SEND_BOTH="\06\011"
+    STOP_TX="\06\012"
     
     GET_FIRMWARE_VERSION = "\06\050"
     
--- setup.py
+++ setup.py
@@ -89,9 +89,21 @@ buildOptions = dict(
 
 setup(
     name = "MobilinkdTnc1Config",
+    version = "0.2",
     author = "Mobilinkd LLC",
-    version = "0.1",
+    author_email = "mobilinkd@gmail.com",
+    url = "https://github.com/mobilinkd/tnc1-python-config",
+    license = "Apache 2.0",
     description = "Configuration tool for Mobilinkd TNC1",
+    long_description = 
+"""This program is used to connect to the Mobilinkd TNC1 via RFCOMM (serial
+port) and is used to set the transmit volume level, monitor receive volume
+level so it can be properly adjusted on the radio, set the KISS parameters,
+and upload new firmware to the TNC.  It requires that the TNC has been
+connected to the computer and assigned a serial port.""",
     options = dict(build_exe = buildOptions),
-    executables = executables
+    platforms = ('Any',),
+    keywords = ('mobilinkd', 'aprs', 'ham', 'afsk', 'tnc'),
+    requires = ['pyserial', 'pygobject3'],
+    scripts = executables
 )
