889672c2708a86c31229ee4cadc7e1b9672f5676
Rob Riggs <rob@pangalactic.org>
2019-06-22 16:52:07 -0500

    Modernize Windows build process in setup.py.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2.7
 
-import os, site, sys
+import os, site, sys, glob
 
 freeze = False
 try:
@@ -12,50 +12,37 @@ except ImportError:
 ## Get the site-package folder, not everybody will install
 ## Python into C:\PythonXX
 site_dir = site.getsitepackages()[1]
-include_dll_path = os.path.join(site_dir, "gtk")
-
-## Collect the list of missing dll when cx_freeze builds the app
-missing_dll = ['libgtk-3-0.dll',
-               'libgdk-3-0.dll',
-               'libatk-1.0-0.dll',
-               'libcairo-gobject-2.dll',
-               'libgdk_pixbuf-2.0-0.dll',
-               'libjpeg-8.dll',
-               'libpango-1.0-0.dll',
-               'libpangocairo-1.0-0.dll',
-               'libpangoft2-1.0-0.dll',
-               'libpangowin32-1.0-0.dll',
-               'libpyglib-gi-2.0-python-0.dll',
-               'libglib-2.0-0.dll',
-               'libgobject-2.0-0.dll',
-               'libintl-8.dll',
-               'libgirepository-1.0-1.dll',
-               'libgio-2.0-0.dll',
-               'libgmodule-2.0-0.dll',
-               'libffi-6.dll',
-               'zlib1.dll',
-               'libcairo-2.dll',
-               'libfontconfig-1.dll',
-               'libfreetype-6.dll',
-               'libxml2-2.dll',
-               'libpng15-15.dll',
-               'libjson-glib-1.0-0.dll',
-               'libgnutls-26.dll',
-               'libgcrypt-11.dll',
-               'libp11-kit-0.dll'
-]
+include_dll_path = os.path.join(site_dir, "gnome")
 
 ## We also need to add the glade folder, cx_freeze will walk
 ## into it and copy all the necessary files
 glade_folder = 'glade'
 
 ## We need to add all the libraries too (for themes, etc..)
-gtk_libs = ['etc', 'lib', 'share']
+gtk_libs = [
+    'etc',
+    'lib/gdbus-2.0',
+    'lib/gdk-pixbuf-2.0',
+    'lib/gio',
+    'lib/girepository-1.0',
+    'lib/glade',
+    'lib/gtk-3.0',
+    'share/dbus-1',
+    'share/fontconfig',
+    'share/fonts',
+    'share/gir-1.0',
+    'share/glade',
+    'share/glib-2.0',
+    'share/icons/Adwaita',
+    'share/themes/Adwaita',
+    # 'share/locale'
+    ]
 
 ## Create the list of includes as cx_freeze likes
 include_files = []
-for dll in missing_dll:
-    include_files.append((os.path.join(include_dll_path, dll), dll))
+
+for dll in glob.glob(os.path.join(include_dll_path, "*.dll")):
+    include_files.append((dll, os.path.basename(dll)))
 
 ## Let's add glade folder and files
 include_files.append((glade_folder, glade_folder))
@@ -71,7 +58,13 @@ if sys.platform == "win32":
     base = "Win32GUI"
 
 if freeze:
-    executables = [Executable("TncConfigApp.py", base=base)]
+    executables = [
+        Executable(
+            "TncConfigApp.py",
+            base=base,
+            shortcutName="Mobilinkd TNC Config (Serial)",
+            shortcutDir="StartMenuFolder"
+    )]
     scripts = None
 else:
     scripts = ['TncConfigApp.py']
@@ -80,26 +73,24 @@ else:
 py_modules = ['Avr109', 'BootLoader', 'IntelHexRecord', 'TncModel']
 
 buildOptions = dict(
-    compressed = False,
     includes = ["gi"],
     packages = ["gi"],
-    include_files = include_files
+    include_files = include_files,
     )
 
 setup(
     name = "TncConfigApp",
-    version = "1.0.1",
+    version = "1.0.2",
     author = "Mobilinkd LLC",
     author_email = "mobilinkd@gmail.com",
     url = "https://github.com/mobilinkd/tnc1-python-config",
     license = "Apache 2.0",
-    description = "Configuration tool for Mobilinkd TNC1, TNC2 and TNC3 (serial version)",
+    description = "Configuration tool for Mobilinkd TNC1, TNC2 and TNC3",
     long_description = 
-"""This program is used to connect to a Mobilinkd TNC via Bluetooth SPP
-(serial port) and is used to set the transmit volume level, monitor receive
+"""This program is used to connect to a Mobilinkd TNC via serial port (including
+Bluetooth SPP) and is used to set the transmit volume level, monitor receive
 volume level so it can be properly adjusted on the radio, set the KISS
-parameters, and upload new firmware to the TNC.  It requires that the TNC has
-been connected to the computer and assigned a serial port.""",
+parameters, and upload new firmware to TNC1 & TNC2 devices.""",
     options = dict(build_exe = buildOptions),
     platforms = ('Any',),
     keywords = ('mobilinkd', 'aprs', 'ham', 'afsk', 'tnc', 'ax25', 'kiss'),
